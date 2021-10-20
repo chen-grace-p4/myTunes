fdae41150c04a05d8c34371077d5d9299240052e
Grace Chen <73039310+chen-grace-p4@users.noreply.github.com>
2021-10-20 14:35:36 +0000

    set up the files

--- main.c
+++ main.c
@@ -5,5 +5,12 @@
 #include "tunesList.h"
 
 int main() {
-  
+	struct song_node *list = NULL;
+	
+
+	printf("LINKED LIST TESTS\n");
+	printf("====================================\n");
+	printf("Testing print_list:\n");
+	print_list(list);
+	
 }
--- myTunes.c
+++ myTunes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct song_node{
   char name[100];
@@ -21,6 +22,44 @@ struct song_node * make_song(char *name, char *artist) {
 struct song_node * insert_front(struct song_node *front, char *name, char *artist) {
     struct song_node *n = make_song(name, artist);
     n->next = front;
+    return n;
+}
+
+struct song_node * insert_order(struct song_node *front, char *name, char *artist) {
+	struct song_node *n = make_song(name, artist);
+    return n;
+}
+
+void print_song(struct song_node * s) {
+   printf("song name: '%s'\tartist: '%s' \n", s->name, s->artist);
+}
+
+void print_list(struct song_node * current) {
+   while (current != NULL) {
+      print_song(current);
+      current = current->next;
+   }
+}
+
+struct song_node * find(struct song_node *s, char *name, char *artist) {
+	struct song_node *n = make_song(name, artist);
+    return n;
+}
+struct song_node * find_artist(struct song_node *s, char *artist) {
+	struct song_node *n = make_song("placeholder", artist);
+    return n;
+}
+
+struct song_node * return_random(struct song_node *front) {
+	struct song_node *n = make_song("placeholder", "placeholder");
+    return n;
+}
+struct song_node * remove_node(struct song_node *s, char *name, char *artist) {
+	struct song_node *n = make_song("placeholder", "placeholder");
+    return n;
+}
+struct song_node *free_list(struct song_node *n) {
+	struct song_node *n = make_song("placeholder", "placeholder");
     return n;
 }
 
